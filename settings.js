7f9044cb693fc1448207c191ac47042332bfde88
Alexander E. Escobar <36279283+Alexander-Escobar@users.noreply.github.com>
2025-03-27 00:35:21 -0600

    Update settings.js
    Correción en valores de configuración

--- settings.js
+++ settings.js
@@ -5,20 +5,17 @@
 		dbConfig	Configuracion de la Base de Datos
 		servConfig	Configuracion del Servidor
 		httpConfig	Configuracion de las Peticiones URL del Protocolo HTTP
-		Google		Configuracion para Google Ads
-		Image		Configuracion General para las imagenes
-		pagConfig	Configuracion de la Paginacion de los Mantenimientos
 */
 
 exports.httpMsgFormat = 'HTML';
 exports.Title = "DB Smarts Docs";
-exports.Rights_Reserved = "2023 &copy; A&C Consultoría Informática";
+exports.Rights_Reserved = "2023-2025 &copy; A&C Consultoría Informática";
 
 exports.dbConfig = 
 {
 	// mysql
-    // host: "localhost",
-      user: "Owlet",
+    // host: "localhost o IP",
+      user: "User",
       password: "Password01",
     // database: "DBSisConta"
       connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=namehostoip)(PORT=port))(CONNECT_DATA=(SERVICE_NAME=servicename)))"
